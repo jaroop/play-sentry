866fa9d236b34925bf8d7402541317bc54873090
Michael Zajac <mzajac@jaroop.com>
2017-08-31 18:02:42 +0000

    JCOR-220 Added tests for AsyncAuth
    Unfortunately `anyObject` triggers many dead code false positives, so I
    disabled the warning, as it will keep growing.

---
+++ core/src/test/scala/com/jaroop/play/sentry/AsyncAuthSpec.scala
@@ -0,0 +1,203 @@
+package test
+
+import com.jaroop.play.sentry._
+import org.mockito.Matchers._
+import org.specs2.concurrent._
+import org.specs2.mock._
+import org.specs2.mutable._
+import play.api.mvc.{ RequestHeader, Result, Results }
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
+
+class AsyncAuthSpec(implicit ee: ExecutionEnv) extends Specification with Mockito {
+
+    def asyncAuthWithMocks = new AsyncAuth[TestEnv] {
+        val config = mock[AuthConfig[TestEnv]]
+        val idContainer = mock[IdContainer[Long]]
+        val tokenAccessor = mock[TokenAccessor]
+    }
+
+    def asyncAuthWithMocksWithUser = new AsyncAuth[TestEnv] {
+        val config = mock[AuthConfig[TestEnv]]
+        val idContainer = mock[IdContainer[Long]]
+        val tokenAccessor = mock[TokenAccessor]
+        override def restoreUser(implicit request: RequestHeader, ec: ExecutionContext): Future[(Option[User], ResultUpdater)] =
+            Future.successful((Option(User.test), identity _))
+    }
+
+    "The AsyncAuth trait" should {
+
+        tag("restoreUser")
+        "successfully restore a user" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option(token))
+            auth.idContainer.get(token).returns(Future.successful(Option(1L)))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.successful(Option(User.test)))
+            auth.idContainer.prolongTimeout(anyObject, anyObject)(anyObject).returns(Future.successful(()))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must beSome(User.test).await
+        }
+
+        tag("restoreUser")
+        "fail to restore a user when they do not have a token" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option.empty[AuthenticityToken])
+            auth.idContainer.get(token).returns(Future.successful(Option(1L)))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.successful(Option(User.test)))
+            auth.idContainer.prolongTimeout(anyObject, anyObject)(anyObject).returns(Future.successful(()))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must beNone.await
+        }
+
+        tag("restoreUser")
+        "fail to restore a user when they have a valid token, but the IdContainer does not recognize it" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option(token))
+            auth.idContainer.get(token).returns(Future.successful(Option.empty[Long]))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.successful(Option(User.test)))
+            auth.idContainer.prolongTimeout(anyObject, anyObject)(anyObject).returns(Future.successful(()))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must throwAn[Exception].await
+        }
+
+        tag("restoreUser")
+        "fail to restore a user when they have a valid token, but the IdContainer completely fails" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option(token))
+            auth.idContainer.get(token).returns(Future.failed(new Exception))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.successful(Option(User.test)))
+            auth.idContainer.prolongTimeout(anyObject, anyObject)(anyObject).returns(Future.successful(()))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must throwAn[Exception].await
+        }
+
+        tag("restoreUser")
+        "fail to restore a user when the user cannot be resolved by ID" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option(token))
+            auth.idContainer.get(token).returns(Future.successful(Option(userId)))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.successful(Option.empty[User]))
+            auth.idContainer.prolongTimeout(anyObject, anyObject)(anyObject).returns(Future.successful(()))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must throwAn[Exception].await
+        }
+
+        tag("restoreUser")
+        "fail to restore a user when the resolver completely fails" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option(token))
+            auth.idContainer.get(token).returns(Future.successful(Option(userId)))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.failed(new Exception))
+            auth.idContainer.prolongTimeout(anyObject, anyObject)(anyObject).returns(Future.successful(()))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must throwAn[Exception].await
+        }
+
+        tag("restoreUser")
+        "fail to restore a user when the IdContainer fails to prolong the session" in {
+            val auth = asyncAuthWithMocks
+            implicit val request = mock[RequestHeader]
+            val token = "secrettoken"
+            val userId = 1L
+            auth.tokenAccessor.extract(request).returns(Option(token))
+            auth.idContainer.get(token).returns(Future.successful(Option(1L)))
+            auth.config.resolveUser(anyObject)(anyObject).returns(Future.successful(Option(User.test)))
+            auth.idContainer.prolongTimeout(token, 1.minutes).returns(Future.failed(new Exception))
+            val result = mock[Result]
+            auth.tokenAccessor.put(token)(result).returns(result)
+            auth.restoreUser.map(_._1) must throwAn[Exception].await
+        }
+
+        tag("authorized")
+        "successfully authorize a user with the proper credentials" in {
+            val auth = asyncAuthWithMocksWithUser
+            implicit val request = mock[RequestHeader]
+            auth.config.authorize(User.test, Admin).returns(Future.successful(true))
+            auth.authorized(Admin).map(_.right.map(_._1)) must beRight(User.test).await
+        }
+
+        tag("authorized")
+        "fail to authorize a user that is not authorized for an authority key" in {
+            val auth = asyncAuthWithMocksWithUser
+            implicit val request = mock[RequestHeader]
+            val result: Result = Results.Forbidden
+            auth.config.authorize(User.test, Admin).returns(Future.successful(false))
+            auth.config.authorizationFailed(anyObject, anyObject, anyObject)(anyObject).returns(
+                Future.successful(result)
+            )
+            auth.authorized(Admin) must beLeft(result).await
+        }
+
+        tag("authorized")
+        "fail to authorize a user when the config authorize function fails completely" in {
+            val auth = asyncAuthWithMocksWithUser
+            implicit val request = mock[RequestHeader]
+            val result: Result = Results.Forbidden
+            auth.config.authorize(User.test, Admin).returns(Future.failed(new Exception))
+            auth.config.authorizationFailed(anyObject, anyObject, anyObject)(anyObject).returns(
+                Future.successful(result)
+            )
+            auth.authorized(Admin) must beLeft(result).await
+        }
+
+        tag("authorized")
+        "fail to authorize a user when they are not authenticated" in {
+            val auth = new AsyncAuth[TestEnv] {
+                val config = mock[AuthConfig[TestEnv]]
+                val idContainer = mock[IdContainer[Long]]
+                val tokenAccessor = mock[TokenAccessor]
+                override def restoreUser(implicit request: RequestHeader, ec: ExecutionContext): Future[(Option[User], ResultUpdater)] =
+                    Future.successful((Option.empty[User], identity _))
+            }
+            implicit val request = mock[RequestHeader]
+            val result: Result = Results.Forbidden
+            auth.config.authenticationFailed(anyObject)(anyObject).returns(
+                Future.successful(result)
+            )
+            auth.authorized(Admin) must beLeft(result).await
+        }
+
+        tag("authorized")
+        "fail to authorize a user when session user restoration fails completely" in {
+            val auth = new AsyncAuth[TestEnv] {
+                val config = mock[AuthConfig[TestEnv]]
+                val idContainer = mock[IdContainer[Long]]
+                val tokenAccessor = mock[TokenAccessor]
+                override def restoreUser(implicit request: RequestHeader, ec: ExecutionContext): Future[(Option[User], ResultUpdater)] =
+                    Future.failed(new Exception)
+            }
+            implicit val request = mock[RequestHeader]
+            val result: Result = Results.Forbidden
+            auth.config.authenticationFailed(anyObject)(anyObject).returns(
+                Future.successful(result)
+            )
+            auth.authorized(Admin) must beLeft(result).await
+        }
+
+    }
+
+}
