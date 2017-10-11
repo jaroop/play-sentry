461a39e0126f7eb05da33d400f147204b84f36fb
mhzajac <mzajac@jaroop.com>
2017-10-11 14:26:26 -0400

    JCOR-228 JCOR-229 JCOR-230 JCOR-233 JCOR-236 JCOR-239 Added scaladoc for various classes
    This adds scaladoc for:
    * The `sentry` package
    * `AsyncAuth`
    * `AuthRequest`
    * `AuthenticatedActionBuilder`
    * `AuthConfig`
    * `Env`
    * `OptionalAuthRequest`
    * `OptionalAuthenticatedActionBuilder`

--- core/src/main/scala/com/jaroop/play/sentry/AsyncAuth.scala
+++ core/src/main/scala/com/jaroop/play/sentry/AsyncAuth.scala
@@ -4,12 +4,33 @@ import javax.inject.Inject
 import play.api.mvc._
 import scala.concurrent.{ExecutionContext, Future}
 
+/**
+ *  Provides the ability to asychronously authenticate and authorize a logged-in user based on the contents of a request.
+ *  There should be little need to use this class directly, as its main purpose is to share code between the action builders.
+ *
+ *  @param config Requires an [[AuthConfig]] for authorization and user resolution.
+ *  @param idContainer Requires an [[IdContainer]] to validate the user's session and prolong it, if successful.
+ *  @param tokenAccessor Requires a [[TokenAccessor]] to retrieve an [[AuthenticityToken]] from the request,
+ *                       or to set a new one when prolonging the session timeout.
+ *
+ *  @tparam E The environment type.
+ */
 class AsyncAuth[E <: Env] @Inject() (
     config: AuthConfig[E],
     idContainer: IdContainer[E#Id],
     tokenAccessor: TokenAccessor
 ) {
 
+    /**
+     *  Restores a user from a request, and determines whether or not that user is authorized to perform an action identified
+     *  with a specific authority key.
+     *
+     *  @param authority The authority key with which to determine whether or not the user is authorized.
+     *  @param request The request used to identify a user by their cookies.
+     *  @return If the user is authorized for the given authority key, the logged-in user is returned tupled with a function
+     *          that will update a result with the user's new cookie (within an `ActionBuilder`).
+     *          If the user is not authorized, then the `Result` from [[AuthConfig#authorizationFailed]] will be returned.
+     */
     def authorized(authority: E#Authority)
         (implicit request: RequestHeader, ec: ExecutionContext): Future[Either[Result, (E#User, ResultUpdater)]] = {
         restoreUser collect {
@@ -26,6 +47,13 @@ class AsyncAuth[E <: Env] @Inject() (
         }
     }
 
+    /**
+     *  Authenticates a user from a request and prolongs their session if successful.
+     *
+     *  @param request The request used to identify a user by their cookies.
+     *  @return An optional user (`Some` if authentication is successful) tupled with a function that will update a result with
+     *          the user's new cookie (if they are actually logged-in, otherwise it does nothing).
+     */
     def restoreUser(implicit request: RequestHeader, ec: ExecutionContext): Future[(Option[E#User], ResultUpdater)] = {
         (for {
             token  <- extractToken(request)
--- core/src/main/scala/com/jaroop/play/sentry/package.scala
+++ core/src/main/scala/com/jaroop/play/sentry/package.scala
@@ -2,12 +2,16 @@ package com.jaroop.play
 
 import play.api.mvc.Result
 
+/** The Play Sentry API. */
 package object sentry {
 
+    /** Play Sentry tokens are generated as strings. */
     type AuthenticityToken = String
 
+    /** Signed Play Sentry tokens are strings. */
     type SignedToken = String
 
+    /** An alias to a function that updates a `Result` with a new `Result`. */
     type ResultUpdater = Result => Result
 
 }
